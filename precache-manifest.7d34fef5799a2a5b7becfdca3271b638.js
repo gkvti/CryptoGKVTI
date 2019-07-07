55498a43a8d0d0f566b76d24ddfb0676a7acf0ad
GKVTI <gkvti@protonmail.com>
2019-07-07 04:11:09 +0300

    Updates

---
+++ precache-manifest.7d34fef5799a2a5b7becfdca3271b638.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "aaa322823534f27395a2349017596715",
+    "url": "/CryptoGKVTI/static/media/logo-te-logo.aaa32282.png"
+  },
+  {
+    "revision": "b074347d9b5f9280bd3c",
+    "url": "/CryptoGKVTI/static/js/runtime~main.b074347d.js"
+  },
+  {
+    "revision": "37f1f614936c53676e5b",
+    "url": "/CryptoGKVTI/static/js/main.37f1f614.chunk.js"
+  },
+  {
+    "revision": "9865ef5823f8f930a80f",
+    "url": "/CryptoGKVTI/static/js/1.9865ef58.chunk.js"
+  },
+  {
+    "revision": "37f1f614936c53676e5b",
+    "url": "/CryptoGKVTI/static/css/main.3fcdecf3.chunk.css"
+  },
+  {
+    "revision": "a1943ed199c1a88eff9434912587e703",
+    "url": "/CryptoGKVTI/index.html"
+  }
+];
